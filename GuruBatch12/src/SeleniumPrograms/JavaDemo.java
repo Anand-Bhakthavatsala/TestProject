b9f5236ead4a29eb57302619ad1a5a7040d3b19d
anand269 <anand269@gmail.com>
2023-08-12 16:19:52 +0530

    one more

---
+++ GuruBatch12/src/SeleniumPrograms/JavaDemo.java
@@ -0,0 +1,20 @@
+package SeleniumPrograms;
+
+public class JavaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To java");
+		
+		String expName="Batch12";
+		String actName="Batch12 - Guruschools";
+		
+		if(actName.equals("Guruschools"))
+		{
+			System.out.println("Both are same");			
+		}
+		else
+			System.out.println("Not same");
+	}
+
+}
